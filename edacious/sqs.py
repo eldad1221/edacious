c61ea5d94b80344bce82597f1ddbaaad289e6953
eldad1221 <eldad@1221tlv.org>
2023-09-19 14:41:45 +0300

    Version 0.0.5: SQS listener fix

--- edacious/sqs.py
+++ edacious/sqs.py
@@ -6,6 +6,9 @@ from quickbelog import Log
 # Create SQS client
 sqs = boto3.client('sqs')
 
+MESSAGE_ATTRIBUTE_NAMES = 'MessageAttributeNames'
+MESSAGE_ATTRIBUTES = 'MessageAttributes'
+
 
 class EventListener(edacious.EventListener):
 
@@ -19,6 +22,7 @@ class EventListener(edacious.EventListener):
         self._sqs_url = sqs_url
         self._visibility_timeout = visibility_timeout
         self._max_messages_to_fetch = max_messages_to_fetch
+        Log.info(f'Will listen to SQS {self._sqs_url}')
         super().__init__(tuple(), {})
 
     def fetch(self) -> list:
@@ -39,11 +43,7 @@ class EventListener(edacious.EventListener):
 
                 try:
                     event = json.loads(msg.get('Body'))
-                    event.update(msg.get('MessageAttributeNames', {}))
-                    event[edacious.EVENT_TYPE_KEY] = msg.get(
-                        'MessageAttributes',
-                        {}
-                    ).get(edacious.EVENT_TYPE_KEY, {}).get('StringValue')
+                    event[edacious.EVENT_TYPE_KEY] = self.get_event_type(msg=msg)
                     event['ReceiptHandle'] = msg['ReceiptHandle']
                     events.append(event)
                 except json.JSONDecodeError:
@@ -51,6 +51,21 @@ class EventListener(edacious.EventListener):
             return events
         else:
             return []
+
+    @staticmethod
+    def get_event_type(msg: dict) -> str:
+        even_type = None
+        if MESSAGE_ATTRIBUTE_NAMES in msg:
+            even_type = msg.get(
+                        'MessageAttributes',
+                        {}
+                    ).get(edacious.EVENT_TYPE_KEY, {}).get('StringValue')
+        elif MESSAGE_ATTRIBUTES in msg.get('Body', {}):
+            even_type = msg.get(
+                'MessageAttributes',
+                {}
+            ).get(edacious.EVENT_TYPE_KEY, {}).get('Value')
+        return even_type
 
     def event_handling_error(self, event: dict):
         pass
